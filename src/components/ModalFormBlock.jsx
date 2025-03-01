3bb1094f92b9b48818196e7b7be31b21431cd67f
Imago31 <xaker201301@gmail.com>
2025-03-01 22:09:19 +0100

    validation

--- src/components/ModalFormBlock.jsx
+++ src/components/ModalFormBlock.jsx
@@ -1,16 +1,16 @@
 import Flatpickr from "react-flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-function ModalFormBlock({modal,setModal,formSubmit,formInputChange,form,timeInputChange}){
+function ModalFormBlock({modal,setModal,formSubmit,formInputChange,form,timeInputChange,validate, valid}){
     return(
         <>
             {modal && 
                 <div className="modal w3-animate-top"> 
                     <button onClick={() =>{setModal(false)}} className="closeModal">&#10005;</button>
                     <form onSubmit={formSubmit} className="form">
-                    <input onChange={formInputChange} value={form.name} name="name" className="inputForm" type="text" placeholder="User name"/>
-                    <input onChange={formInputChange} value={form.appointment} name="appointment" className="inputForm" type="text" placeholder="Appointment name"/>
-                    <input onChange={formInputChange} value={form.date} name="date" className="inputForm" type="date" placeholder="Date"/>
+                    <input onChange={formInputChange} value={form.name} name="name" className="inputForm" type="text" placeholder="User name" style={{borderLeft: valid.name && '2px solid red'}}/>
+                    <input onChange={formInputChange} value={form.appointment} name="appointment" className="inputForm" type="text" placeholder="Appointment name" style={{borderLeft: valid.appointment && '2px solid red'}}/>
+                    <input onChange={formInputChange} value={form.date} name="date" className="inputForm" type="date" placeholder="Date" style={{borderLeft: valid.date && '2px solid red'}}/>
                     <Flatpickr
                             name="time"
                             value={form.time}
@@ -23,7 +23,9 @@ function ModalFormBlock({modal,setModal,formSubmit,formInputChange,form,timeInpu
                                 time_24hr: true,
                             }}
                             className="inputForm"
+                            style={{borderLeft: valid.time && '2px solid red'}}
                     />
+                    {Object.keys(valid).length !== 0 && <p style={{color: 'red'}}>All fields are required</p>}
                     <button className="buttonForm" type="submit">Add appointment</button>
                     </form>
                 </div>
